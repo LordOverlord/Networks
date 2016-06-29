879da7e8622b29a6377e29b000d5bd3608461665
Rafael <barto_et@hotmail.com>
2016-06-29 00:06:26 -0500

    Update Hostednetworkup.bat
    Edited the info of the hosted network

--- Windows/Hostednetworkup.bat
+++ Windows/Hostednetworkup.bat
@@ -3,9 +3,9 @@ REM First, turn off the output
 @echo off
 REM Set the properties of the network
 REM SSID: replace for the name of your network
-REM key: replace for something you remember
+REM key: replace for something you remember, must use at least 8 characters. Aa8/
 REM keyusage: this set the key for later use
-netsh wlan set hostednetwork mode=allow ssid=Overlord key=12345678 keyUsage=persistent
+netsh wlan set hostednetwork mode=allow ssid=Example key=01234567 keyUsage=persistent
 REM after adjust the hostednetwork, the following start the same
 netsh wlan start hostednetwork
 REM Show a message to the user, about the network is up
